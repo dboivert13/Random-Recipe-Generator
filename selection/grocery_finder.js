568e270bb897a2f2af85d53668746de87358d8f0
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-02 19:35:31 -0500

    added results label to grocery search and wrote string validation for both findGrocery and setHome functions

--- selection/grocery_finder.js
+++ selection/grocery_finder.js
@@ -1,48 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
     function findGrocery() {
         // Empty ul div so the list doesn't keep getting longer and longer
         $("#nearest-stores").empty();
         // Grabbing value of search input and inputting that into initial AJAX call for geocode API
         var address = $("#user-location").val();
         var addQueryUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=pk.eyJ1Ijoic21jbWlsbGFuMjgiLCJhIjoiY2trazBsMTRmMDIyMjJvcHJ6Nzk5bGF4cCJ9.ksKKrAuT0a0V7QJP1Q-YgQ";
-        
-        $.ajax({
-            url: addQueryUrl,
-            method: "GET"   
-        }).then(function(res) {
-            console.log(res);
-            // Isolating lat/lon from initial AJAX call to use for POI proximity search
-            var lon = res.features[0].geometry.coordinates[0];
-            var lat = res.features[0].geometry.coordinates[1];
-            console.log(lon);
-            console.log(lat);
-            
-            // Use lat/lon results in second AJAX call
-            var groQueryUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/groceries.json?proximity=" + lon + "," + lat + "&access_token=pk.eyJ1Ijoic21jbWlsbGFuMjgiLCJhIjoiY2trazBsMTRmMDIyMjJvcHJ6Nzk5bGF4cCJ9.ksKKrAuT0a0V7QJP1Q-YgQ";
+
+        if (address == "") {
+            return;
+        } else {
             $.ajax({
-                url: groQueryUrl,
+                url: addQueryUrl,
                 method: "GET"
-            }).then(function(response) {
-                console.log(response);
-                for (var i = 0; i < 5; i++) {
-                    var store = $("<li>").text(response.features[i].place_name);
-                    $("#nearest-stores").append(store);
-                }
+            }).then(function (res) {
+                console.log(res);
+                // Isolating lat/lon from initial AJAX call to use for POI proximity search
+                var lon = res.features[0].geometry.coordinates[0];
+                var lat = res.features[0].geometry.coordinates[1];
+                console.log(lon);
+                console.log(lat);
+
+                // Use lat/lon results in second AJAX call
+                var groQueryUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/groceries.json?proximity=" + lon + "," + lat + "&access_token=pk.eyJ1Ijoic21jbWlsbGFuMjgiLCJhIjoiY2trazBsMTRmMDIyMjJvcHJ6Nzk5bGF4cCJ9.ksKKrAuT0a0V7QJP1Q-YgQ";
+                $.ajax({
+                    url: groQueryUrl,
+                    method: "GET"
+                }).then(function (response) {
+                    console.log(response);
+                    for (var i = 0; i < 5; i++) {
+                        $("#search-result-label").text("Showing Results For: " + res.features[0].place_name);
+                        var store = $("<li>").text(response.features[i].place_name);
+                        $("#nearest-stores").append(store);
+                    }
+                })
+                $("#user-location").val("");
             })
-            $("#user-location").val(""); 
-        }) 
+        }
     }
 
     $("#grocery-search").click(findGrocery);
 
+    // Writing function for user to save home address for future searches
     function setHome() {
         var homeAddress = $("#user-location").val();
-        localStorage.setItem("home-address", JSON.stringify(homeAddress));
-        $("#user-location").val(""); 
+        if (homeAddress == "") {
+            return;
+        } else {
+            localStorage.setItem("home-address", JSON.stringify(homeAddress));
+            $("#user-location").val("");
+        }
     }
 
     $("#set-home").click(setHome);
 
+    // Writing function that will pull home address from local storage and run the findGrocery function
     function homeSearch() {
         $("#user-location").val(JSON.parse(localStorage.getItem("home-address")));
         findGrocery();
